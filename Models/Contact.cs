baa774f57ca2f78c6b0008389413286755b77b6f
Mayank Srivastava <M.Srivastava2013@outlook.com>
2024-10-22 04:06:25 +0530

    First Model Contact.

---
+++ Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net_Framework.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public Contact()
+        {
+            Id = -1;
+            firstName = string.Empty;
+            lastName = string.Empty;
+        }
+    }
+}
