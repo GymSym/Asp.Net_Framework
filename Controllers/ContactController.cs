d72a79e5084585661746b17bf237c713626be299
Mayank Srivastava <M.Srivastava2013@outlook.com>
2024-10-22 13:30:00 +0530

    Completed the ASP.Net Framework application with Ajax calls for CRUD operation.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,80 @@
+﻿using Asp.Net_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Asp.Net_Framework.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact() { Id=0, firstName="Mayank", lastName="Srivastava"},
+            new Contact() { Id=1, firstName="Shamya", lastName="Sharma"},
+            new Contact() { Id=2, firstName="Meet", lastName="Kaur"}
+        };
+
+        // GET: api/Contact
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+
+        }
+
+
+        // GET: api/Contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contact contact = contacts.FirstOrDefault<Contact>(c => c.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+
+        }
+
+        // POST: api/Contact
+        public IEnumerable<Contact> Post([FromBody] Contact newContact)
+        {
+            List<Contact> contactsList = contacts.ToList<Contact>();
+            newContact.Id = contactsList.Count;
+            contactsList.Add(newContact);
+            contacts = contactsList.ToArray();
+
+            return contacts;
+
+
+        }
+
+        // PUT: api/Contact/5
+        public IEnumerable<Contact> Put(int id, [FromBody] Contact updateContact)
+        {
+            Contact contact = contacts.FirstOrDefault<Contact>(c=>c.Id==id);
+            if(contact != null)
+            {
+                contact.firstName = updateContact.firstName;
+                contact.lastName = updateContact.lastName;
+
+            }
+           
+
+            return contacts;
+        }
+
+        // DELETE: api/Contact/5
+        public IEnumerable<Contact> Delete(int id)
+        {
+            
+            contacts = contacts.Where<Contact>(c => c.Id !=id).ToArray<Contact>();
+
+
+
+            return contacts;
+        }
+    }
+}
---
+++ Data/AspNet_FrameworkContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net_Framework.Data
+{
+    public class AspNet_FrameworkContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AspNet_FrameworkContext() : base("name=AspNet_FrameworkContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Asp.Net_Framework.Models.Contact> Contacts { get; set; }
+    }
+}
